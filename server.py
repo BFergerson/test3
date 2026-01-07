1f6047cab07f7a79329813289a488c3caec51c04
Brandon Fergerson <bfergerson@apache.org>
2026-01-06 23:22:05 -0500

    test

--- server.py
+++ server.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
 
 
         if __name__ == "__main__":
-            app.run(port=8080)
+            app.run(host="0.0.0.0", port=8080)
 
     else:
         asyncio.run(main())
