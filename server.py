adc9531a30f841d9001d32c01d70dccf318953d2
Brandon Fergerson <bfergerson@apache.org>
2026-01-06 23:14:47 -0500

    test

--- server.py
+++ server.py
@@ -392,4 +392,19 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    if os.getenv("MB_VIEWER") == "true":
+        from flask import Flask, send_from_directory
+
+        app = Flask(__name__)
+
+
+        @app.route("/")
+        def index():
+            return send_from_directory("games/nmmo3", "game.html")
+
+
+        if __name__ == "__main__":
+            app.run(port=8080)
+
+    else:
+        asyncio.run(main())
